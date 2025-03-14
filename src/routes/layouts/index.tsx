00dfd40e93558018bd25f65d236e68b89f6a90d9
jinwoo710 <jinwoo710@naver.com>
2025-03-14 16:01:54 +0900

    반응형 레이아웃

--- src/routes/layouts/index.tsx
+++ src/routes/layouts/index.tsx
@@ -2,9 +2,8 @@ import { Outlet } from 'react-router-dom'
 
 export default function Layout() {
   return (
-    <>
-      <div>sdfjlksdjf</div>
+    <div className="mx-auto px-4 lg:px-0 w-full max-w-screen-md lg:max-w-screen-lg text-dark bg-orange-500  min-h-screen">
       <Outlet />
-    </>
+    </div>
   )
 }
